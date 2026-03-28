4b6137bb875fc8c988e5af383ace71d045997dc7
alab1020 <hakimalmestrehi@gmail.com>
2026-03-29 00:22:22 +0100

    Clear Cart

--- Frontend/src/context/Cart/CartProvider.tsx
+++ Frontend/src/context/Cart/CartProvider.tsx
@@ -1,4 +1,5 @@
-import { type FC, type PropsWithChildren, useEffect, useState } from "react";
+
+import { type FC,type PropsWithChildren, useEffect, useState } from "react";
 import { CartContext } from "./CartContext";
 import {type CartItem } from "../../types/CartItem";
 import { BASE_URL } from "../../constants/baseUrl";
@@ -191,9 +192,42 @@ const CartProvider: FC<PropsWithChildren> = ({ children }) => {
     }
   };
 
+  const clearCart = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/cart`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        setError("Failed to empty to cart");
+      }
+
+      const cart = await response.json();
+
+      if (!cart) {
+        setError("Failed to parse cart data");
+      }
+
+      setCartItems([]);
+      setTotalAmount(0);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <CartContext.Provider
-      value={{ cartItems, totalAmount, addItemToCart, updateItemInCart, removeItemInCart }}
+      value={{
+        cartItems,
+        totalAmount,
+        addItemToCart,
+        updateItemInCart,
+        removeItemInCart,
+        clearCart,
+      }}
     >
       {children}
     </CartContext.Provider>
