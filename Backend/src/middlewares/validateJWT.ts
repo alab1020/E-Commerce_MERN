197314eac0168abb36603de9f359eb55f1874643
alab1020 <hakimalmestrehi@gmail.com>
2026-06-19 20:27:42 +0200

    .

--- Backend/src/middlewares/validateJWT.ts
+++ Backend/src/middlewares/validateJWT.ts
@@ -22,11 +22,16 @@ const validateJWT = (req: ExtendedRequest, res: Response, next: NextFunction) =>
         res.status(401).send("Forbidden: No token provided");
         return;
     }
-    jwt.verify(token, process.env.JWT_SECRET || '', async(err, payload) => {
-        if (err) {
-            res.status(403).send("Forbidden: Invalid token");
-            return;
-        }
+    if (!process.env.JWT_SECRET) {              // Check if JWT_SECRET is defined
+    res.status(500).send("Server misconfiguration: JWT_SECRET missing");
+    return;
+}
+jwt.verify(
+    token,
+    process.env.JWT_SECRET,
+    { algorithms: ["HS256"] },
+    async (err, payload) => {
+
         if(!payload){res.status(403).send("Forbidden: Invalid token"); return;}
 
         const userPayload = payload as 
